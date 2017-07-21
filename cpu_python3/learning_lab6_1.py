2c93084ef275351a7798533f5d38415421ca7134
ndukwon <ndukwon@gmail.com>
2017-07-21 19:01:36 +0900

    learning lab6

--- cpu_python3/learning_lab6_1.py
+++ cpu_python3/learning_lab6_1.py
@@ -1,6 +1,18 @@
 # Softmax
+'''
+예측모델 구성 단계
+1. X, Y
+2. H(X) = WX + b (= logits)
+3. Softmax(H(X)) = S(y.i) = e^(y.i) / (∑ e^y)
+4. One-hot encoding
 
-import TensorFlow as tf
+Cost 함수 구성
+1. Softmax(H(X)) (= 예측모델 3단계)
+2. Y값
+3. Cross-Entropy = D(S, L) = - ∑ L.i * log(S.i)
+'''
+
+import tensorflow as tf
 
 x_data = [[1, 2, 1, 1],
           [2, 1, 3, 2],
@@ -23,8 +35,38 @@ X = tf.placeholder("float", [None, 4])
 Y = tf.placeholder("float", [None, 3])
 nb_classes = 3
 
-W = tf.Variable(tf.random_normal([4, nb_classes], name='weight'))
-b = tf.Variable(tf.random_normal(nb_classes), name='bias')
+W = tf.Variable(tf.random_normal([4, nb_classes]), name='weight')
+b = tf.Variable(tf.random_normal([nb_classes]), name='bias')
 
 logits = tf.matmul(X, W) + b
 hypothesis = tf.nn.softmax(logits)
+
+cross_entropy = -tf.reduce_sum(Y * tf.log(hypothesis), axis=1)
+cost = tf.reduce_mean(cross_entropy)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(2001):
+        sess.run(optimizer, feed_dict={X:x_data, Y:y_data})
+
+        if step % 200 == 0:
+            print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}))
+
+    print('-----------------------')
+    a = sess.run(hypothesis, feed_dict={X: [[1, 11, 7, 9]]})
+    print(a, sess.run(tf.arg_max(a, 1)))
+
+    print('-----------------------')
+    b = sess.run(hypothesis, feed_dict={X: [[1, 3, 4, 3]]})
+    print(b, sess.run(tf.arg_max(b, 1)))
+
+    print('-----------------------')
+    c = sess.run(hypothesis, feed_dict={X: [[1, 1, 0, 1]]})
+    print(c, sess.run(tf.arg_max(c, 1)))
+
+    print('-----------------------')
+    all = sess.run(hypothesis, feed_dict={X: [[1, 11, 7, 9], [1, 3, 4, 3], [1, 1, 0, 1]]})
+    print(all, sess.run(tf.arg_max(all, 1)))
