20b602abfb8702e7462acf24ba0ef58c71edb43d
ndukwon <ndukwon@gmail.com>
2017-07-24 22:00:28 +0900

    learning lab7

---
+++ cpu_python3/learning_lab7_1.py
@@ -0,0 +1,8 @@
+# TensorFlow tips
+'''
+1. Overshooting: learning rate 너무크게 설정되어 Gradient Descent Algorithm을 적용해도 최적을 찾지 못하고 거꾸로 올라가는 현상
+2. Small learning rate: 너무 조금씩 이동해서 시간이 오래걸리거나, 최적을 찾기도 전에 멈추게 된다
+3.
+'''
+
+import tensorflow as tf
