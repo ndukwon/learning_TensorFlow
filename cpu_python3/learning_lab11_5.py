984bf1f9e0cfb2c0e242a2eac08138bbb93e9d10
ndukwon <ndukwon@gmail.com>
2017-10-09 22:29:07 +0900

    learning lab11

--- cpu_python3/learning_lab11_5.py
+++ cpu_python3/learning_lab11_5.py
@@ -9,6 +9,7 @@ deep CNN 설계
     - ReLU
 1-2. Max pool1
     - 2x2 Filter, stride = 2x2 ==> output: 14x14 size x 32개
+1-3. dropout1
 
 2. Layer 2: CNN + Max pool
 2-0. Input: 14x14 size 32개 이미지
@@ -18,6 +19,7 @@ deep CNN 설계
     - ReLU
 2-2. Max pool2
     - 2x2 Filter, stride = 2x2 ==> output: 7x7 size x 64개
+2-3. dropout2
 
 3. Layer 3: CNN + Max pool
 3-0. Input: 7x7 size 64개 이미지
@@ -27,12 +29,14 @@ deep CNN 설계
     - ReLU
 3-2. Max pool3
     - 2x2 Filter, stride = 2x2 ==> output: 4x4 size x 128개
+3-3. dropout3
 
 4. Fully Connected Layer 1
 4-0. Input: 4x4x128=2048
 4-1. Logistic Classification 과 유사하게 결과값 연결
     - WX + b ==> Output: 625개
     - ReLU
+4-2. dropout4
 
 5. Fully Connected Layer 2
 5-0. Input: 625
@@ -59,66 +63,82 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 X = tf.placeholder(tf.float32, [None, 784])             # X: m * 784 Matrix
 Y = tf.placeholder(tf.float32, [None, 10])              # Y: m * 10 Matrix
+keep_prob = tf.placeholder(tf.float32)
 
 # 1-1. CNN1
-cnn1_input = tf.reshape(X, [-1, 28, 28, 1])             # 28x28x1
-cnn1_W = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
+layer1_cnn_input = tf.reshape(X, [-1, 28, 28, 1])             # 28x28x1
+layer1_cnn_W = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
 
-cnn1_stride = [1, 1, 1, 1]
-cnn1_padding = 'SAME'
-cnn1_out = tf.nn.conv2d(cnn1_input, cnn1_W, strides=cnn1_stride, padding=cnn1_padding)  # 28x28x32
-cnn1_out = tf.nn.relu(cnn1_out)
+layer1_cnn_stride = [1, 1, 1, 1]
+layer1_cnn_padding = 'SAME'
+layer1_cnn_out = tf.nn.conv2d(layer1_cnn_input, layer1_cnn_W, strides=layer1_cnn_stride, padding=layer1_cnn_padding)  # 28x28x32
+layer1_cnn_out = tf.nn.relu(layer1_cnn_out)
 
 # 1-2. Max pool1
-pool1_ksize = [1, 2, 2, 1]
-pool1_stride = [1, 2, 2, 1]
-pool1_padding = 'SAME'
-pool1_out = tf.nn.max_pool(cnn1_out, ksize=pool1_ksize, strides=pool1_ksize, padding=pool1_padding) # 14x14x32
+layer1_pool_ksize = [1, 2, 2, 1]
+layer1_pool_stride = [1, 2, 2, 1]
+layer1_pool_padding = 'SAME'
+layer1_pool_out = tf.nn.max_pool(layer1_cnn_out, ksize=layer1_pool_ksize, strides=layer1_pool_stride, padding=layer1_pool_padding) # 14x14x32
+
+# 1-3. dropout1
+layer1_dropout = tf.nn.dropout(layer1_pool_out, keep_prob=keep_prob)
 
 
 # 2-1. CNN2
-cnn2_W = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
+layer2_cnn_W = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
 
-cnn2_stride = [1, 1, 1, 1]
-cnn2_padding = 'SAME'
-cnn2_out = tf.nn.conv2d(pool1_out, cnn2_W, strides=cnn2_stride, padding=cnn2_padding)   # 14x14x64
-cnn2_out = tf.nn.relu(cnn2_out)
+layer2_cnn_stride = [1, 1, 1, 1]
+layer2_cnn_padding = 'SAME'
+layer2_cnn_out = tf.nn.conv2d(layer1_dropout, layer2_cnn_W, strides=layer2_cnn_stride, padding=layer2_cnn_padding)   # 14x14x64
+layer2_cnn_out = tf.nn.relu(layer2_cnn_out)
 
 # 2-2. Max pool2
-pool2_ksize = [1, 2, 2, 1]
-pool2_stride = [1, 2, 2, 1]
-pool2_padding = 'SAME'
-pool2_out = tf.nn.max_pool(cnn2_out, ksize=pool2_ksize, strides=pool2_ksize, padding=pool2_padding) # 7x7x64
+layer2_pool_ksize = [1, 2, 2, 1]
+layer2_pool_stride = [1, 2, 2, 1]
+layer2_pool_padding = 'SAME'
+layer2_pool_out = tf.nn.max_pool(layer2_cnn_out, ksize=layer2_pool_ksize, strides=layer2_pool_stride, padding=layer2_pool_padding) # 7x7x64
+
+# 2-3. dropout2
+layer2_dropout = tf.nn.dropout(layer2_pool_out, keep_prob=keep_prob)
+
 
 # 3-1. CNN3
-cnn3_W = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
+layer3_cnn_W = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
 
-cnn3_stride = [1, 1, 1, 1]
-cnn3_padding = 'SAME'
-cnn3_out = tf.nn.conv2d(pool2_out, cnn3_W, strides=cnn3_stride, padding=cnn3_padding)   # 7x7x128
-cnn3_out = tf.nn.relu(cnn3_out)
+layer3_cnn_stride = [1, 1, 1, 1]
+layer3_cnn_padding = 'SAME'
+layer3_cnn_out = tf.nn.conv2d(layer2_dropout, layer3_cnn_W, strides=layer3_cnn_stride, padding=layer3_cnn_padding)   # 7x7x128
+layer3_cnn_out = tf.nn.relu(layer3_cnn_out)
 
 # 3-2. Max pool3
-pool3_ksize = [1, 2, 2, 1]
-pool3_stride = [1, 2, 2, 1]
-pool3_padding = 'SAME'
-pool3_out = tf.nn.max_pool(cnn3_out, ksize=pool3_ksize, strides=pool3_ksize, padding=pool3_padding) # 4x4x128
+layer3_pool_ksize = [1, 2, 2, 1]
+layer3_pool_stride = [1, 2, 2, 1]
+layer3_pool_padding = 'SAME'
+layer3_pool_out = tf.nn.max_pool(layer3_cnn_out, ksize=layer3_pool_ksize, strides=layer3_pool_stride, padding=layer3_pool_padding) # 4x4x128
+
+# 3-3. dropout3
+layer3_dropout = tf.nn.dropout(layer3_pool_out, keep_prob=keep_prob)
+
 
 # 4-1. Logistic Classification 과 유사하게 결과값 연결
-fc1_input = tf.reshape(pool3_out, [-1, 4 * 4 * 128]) # 2048
-fc1_W = tf.get_variable("fc1_W", shape=[4 * 4 * 128, 625], initializer=tf.contrib.layers.xavier_initializer())
-fc1_b = tf.Variable(tf.random_normal([625]))
+layer4_fc_input = tf.reshape(layer3_dropout, [-1, 4 * 4 * 128]) # 2048
+layer4_fc_W = tf.get_variable("layer4_fc_W", shape=[4 * 4 * 128, 625], initializer=tf.contrib.layers.xavier_initializer())
+layer4_fc_b = tf.Variable(tf.random_normal([625]))
 
-fc1_out = tf.matmul(fc1_input, fc1_W) + fc1_b
-fc1_out = tf.nn.relu(fc1_out)
+layer4_fc_out = tf.matmul(layer4_fc_input, layer4_fc_W) + layer4_fc_b
+layer4_fc_out = tf.nn.relu(layer4_fc_out)
+
+# 4-2. dropout4
+layer4_dropout = tf.nn.dropout(layer4_fc_out, keep_prob=keep_prob)
+
 
 # 5-1. Logistic Classification 과 유사하게 결과값 연결
-fc2_W = tf.get_variable("fc2_W", shape=[625, 10], initializer=tf.contrib.layers.xavier_initializer())
-fc2_b = tf.Variable(tf.random_normal([10]))
+layer5_fc_W = tf.get_variable("layer5_fc_W", shape=[625, 10], initializer=tf.contrib.layers.xavier_initializer())
+layer5_fc_b = tf.Variable(tf.random_normal([10]))
 
-fc2_out = tf.matmul(fc1_out, fc2_W) + fc2_b
+layer5_fc_out = tf.matmul(layer4_dropout, layer5_fc_W) + layer5_fc_b
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=fc2_out, labels=Y))
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=layer5_fc_out, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 
@@ -133,7 +153,7 @@ for epoch in range(training_epochs):
 
     for i in range(total_batch_count):
         batch_x, batch_y = mnist.train.next_batch(batch_size)
-        c, _ = sess.run([cost, optimizer], feed_dict={X: batch_x, Y: batch_y})
+        c, _ = sess.run([cost, optimizer], feed_dict={X: batch_x, Y: batch_y, keep_prob: 0.7})
         avg_cost += c / total_batch_count
 
     print('Epoch:', '%04d' % (epoch + 1), 'cost=', '{:.9f}'.format(avg_cost))
@@ -142,15 +162,15 @@ print('Learning Finished!')
 
 
 # Validation
-correct_prediction = tf.equal(tf.argmax(fc2_out, 1), tf.argmax(Y, 1))
+correct_prediction = tf.equal(tf.argmax(layer5_fc_out, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print('Accuracy:', sess.run(accuracy, feed_dict={
-      X: mnist.test.images, Y: mnist.test.labels}))
+      X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1}))
 
 r = random.randint(0, mnist.test.num_examples - 1)
 print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
 print("Prediction: ", sess.run(
-    tf.argmax(fc2_out, 1), feed_dict={X: mnist.test.images[r:r + 1]}))
+    tf.argmax(layer5_fc_out, 1), feed_dict={X: mnist.test.images[r:r + 1], keep_prob: 1}))
 
 
 '''
@@ -174,4 +194,28 @@ Learning Finished!
 Accuracy: 0.9906
 Label:  [1]
 Prediction:  [1]
+
+
+# dropout 적용 후
+
+Learning started. It takes sometime.
+Epoch: 0001 cost= 0.370057208
+Epoch: 0002 cost= 0.097895388
+Epoch: 0003 cost= 0.072757328
+Epoch: 0004 cost= 0.059838751
+Epoch: 0005 cost= 0.053641720
+Epoch: 0006 cost= 0.045906441
+Epoch: 0007 cost= 0.043834122
+Epoch: 0008 cost= 0.039287253
+Epoch: 0009 cost= 0.034470695
+Epoch: 0010 cost= 0.033388691
+Epoch: 0011 cost= 0.031786095
+Epoch: 0012 cost= 0.029760598
+Epoch: 0013 cost= 0.028764733
+Epoch: 0014 cost= 0.026594039
+Epoch: 0015 cost= 0.027003375
+Learning Finished!
+Accuracy: 0.9929
+Label:  [7]
+Prediction:  [7]
 '''
